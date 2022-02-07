c8807ab06be1f3d9b0ee593dfd9d62c5e569b145
GRUDE <kristoffenbg@gmail.com>
2022-02-07 16:03:02 +0100

    make example components and containers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FrontPage from "./containers/FrontPage/FrontPage";
 import './App.css';
 
 function App() {
   return (
-    <p>APPskrift</p>
+    <Router>
+      <p>Navbar</p>
+      <div style={{backgroundColor: "lightgray"}}>
+        <Switch>
+          <Route exact path="/">
+            <FrontPage/>
+          </Route>
+          <Route path="/my-recepies/">
+            <p>My recepies page</p>
+          </Route>
+        </Switch>
+      </div>
+      <p>Footer component</p>
+  </Router>
   );
 }
 
---
+++ frontend/src/components/ExampleComponent/README.md
@@ -0,0 +1,2 @@
+Folder for an example component. Explanation of the properties and uses of the commponent.
+Made by Kristoffer Grude
