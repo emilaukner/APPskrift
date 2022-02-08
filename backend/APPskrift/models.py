71d7fcb3a501e523040fc3351b3a32f0018f2364
thea <thea.kvinnegard@gmail.com>
2022-02-08 16:58:30 +0100

    make models for recipe, user, category and comment

--- backend/APPskrift/models.py
+++ backend/APPskrift/models.py
@@ -1,3 +1,5 @@
+from tkinter import CASCADE
+import uuid
 from django.db import models
 
 # Create your models here.
@@ -9,3 +11,48 @@ class APPSkrift(models.Model):
 
     def _str_(self):
         return self.title
+
+class User(models.Model): 
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    admin = models.BooleanField(default=False, blank=False)
+    username = models.CharField(max_length=50, unique=True, blank=False)
+    email = models.EmailField(max_length=254, unique=True)
+    darkMode = models.BooleanField(default=False)
+    favorits = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+
+    def _str_(self):
+        return self.username
+
+class Recipe(models.Model):
+    DIFFICULTIES = (
+        ('E', 'Easy'), 
+        ('M', 'Medium'),
+        ('H', 'Hard'),
+    )
+
+    recipeId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    madeBy = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
+    title = models.CharField(max_length=120, blank=False)
+    difficulty = models.CharField(max_length=1, choices=DIFFICULTIES, default='E', blank=False)
+    estimate = models.IntegerField(blank=False)
+    ingredients = models.TextField(blank=False)
+    steps = models.TextField(blank=False)
+    dateMade = models.DateField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, blank=False)
+
+    def _str_(self):
+        return self.title
+
+class Category(models.Model): 
+    categoryId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50, blank=False)
+
+    def __str__(self): 
+        return self.title
+
+class Comment(models.Model):
+    commentId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    comment = models.TextField(blank=False)
+
+
+
