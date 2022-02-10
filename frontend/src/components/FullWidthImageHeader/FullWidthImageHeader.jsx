40500ecd8809c97da3e343ea112d9a094ccfef69
Mie Fyksen <miebf@stud.ntnu.no>
2022-02-10 15:08:04 +0100

    Created full width header component (with props).

--- frontend/src/components/FullWidthImageHeader/FullWidthImageHeader.jsx
+++ frontend/src/components/FullWidthImageHeader/FullWidthImageHeader.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyledDiv } from "./style";
-//import Food2 from "../../assets/Food2.png";
 
 const FullWidthImageHeader = (props) => {
   return (
@@ -8,13 +7,9 @@ const FullWidthImageHeader = (props) => {
       <StyledDiv>
         <div>
             <img src={ props.imgHeader } style={{width:"100%", height:220, objectFit:"cover"}}></img>
-            
         </div>
-            
       </StyledDiv>
     </>
   );
 };
-
 export default FullWidthImageHeader;
-//<img src={ Food2 } style={{width:"100%", height:220, objectFit:"cover"}}></img>
