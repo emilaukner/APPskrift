76faf1783f2924746c3529794538296ba46cbc16
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-02-10 14:10:00 +0100

    Add recipe serializer

--- backend/APPskrift/serializers.py
+++ backend/APPskrift/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Recipe
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('userId', 'admin', 'username', 'password', 'email', 'darkMode', 'favorites')
+
+class RecipeSerializer(serializers.ModelSerializer):
+		class Meta:
+				model = Recipe
+				fields = (
+					"recipeId",
+					"title",
+					"difficulty",
+					"estimate",
+					"ingredients",
+					"steps",
+					"dateMade",
+					"category",
+					"publishedBy"
+				)
