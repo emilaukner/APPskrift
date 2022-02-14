045aefab8c688d41933d0e918403ec16843ca9fb
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-02-14 18:43:01 +0100

    Use drf-nested for requests
    Add drf-nested for requests to the server. Simply adding GET,PUT, POST, DELETE capabilities to every endpoint without much manual work
    
    Author: vegasj@stud.ntnu.no
    Issue: #3

--- backend/APPskrift/views.py
+++ backend/APPskrift/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .serializers import UserSerializer, RecipeSerializer, CategorySerializer, CommentSerializer, EvaluationSerializar
 from .models import User, Recipe, Category, Comment, Evaluation
@@ -9,79 +9,24 @@ from rest_framework.response import Response
 
 from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+# Using ModelViewSet. See https://testdriven.io/blog/drf-views-part-3/ for details
 
-class UserView(APIView):
-	def get(self, request):
-		queryset = User.objects.all()
-		serializer = UserSerializer(queryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
-	
-	def post(self, request):
-		data = JSONParser().parse(request)
-		serializer = UserSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse({"status":"ok"}, status=201)
-		print(serializer.errors)
-		return JsonResponse({"status":"Failed"}, status=400)
+class UserView(viewsets.ModelViewSet):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
 
-class RecipeView(APIView):
-	def get(self, request):
-		queryset = Recipe.objects.all()
-		serializer = RecipeSerializer(queryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
-	
-	def post(self, request):
-		data = JSONParser().parse(request)
-		serializer = RecipeSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse({"status":"ok"}, status=201)
-		print(serializer.errors)
-		return JsonResponse({"status":"Failed"}, status=400)
+class RecipeView(viewsets.ModelViewSet):
+	serializer_class = RecipeSerializer
+	queryset = Recipe.objects.all()
 
-class CategoryView(APIView): 
-	def get(self, request):
-		queryset = Category.objects.all()
-		serializer = CategorySerializer(queryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
+class CategoryView(viewsets.ModelViewSet): 
+	serializer_class = CategorySerializer
+	queryset = Category.objects.all()
 
-	def post(self, request):
-		data = JSONParser().parse(request)
-		serializer = CategorySerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse({"status":"ok"}, status=201)
-		print(serializer.errors)
-		return JsonResponse({"status":"Failed"}, status=400)
+class CommentView(viewsets.ModelViewSet):
+	serializer_class = CommentSerializer
+	queryset = Comment.objects.all()
 
-class CommentView(APIView):
-	def get(self, request):
-		queryset = Comment.objects.all()
-		serializer = CommentSerializer(queryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
-	
-	def post(self, request):
-		data = JSONParser().parse(request)
-		serializer = CommentSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse({"status":"ok"}, status=201)
-		print(serializer.errors)
-		return JsonResponse({"status":"Failed"}, status=400)
-
-class EvaluationView(APIView):
-	def get(self, request):
-		queryset = Evaluation.objects.all()
-		serializer = EvaluationSerializar(queryset, many=True)
-		return JsonResponse(serializer.data, safe=False)
-	
-	def post(self, request):
-		data = JSONParser().parse(request)
-		serializer = EvaluationSerializar(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse({"status":"ok"}, status=201)
-		print(serializer.errors)
-		return JsonResponse({"status":"Failed"}, status=400)
+class EvaluationView(viewsets.ModelViewSet):
+	serializer_class = EvaluationSerializar
+	queryset = Evaluation.objects.all()
