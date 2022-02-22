ef6f8c52b50870d56f998c491c45bc00f6035c05
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-02-22 15:54:35 +0100

    Sample login created

---
+++ frontend/src/components/LogInPopUp/LoginPopUp.style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledLoginPopup = styled.div`
+	position: absolute;
+	left: 50%;
+  transform: translate(-50%, 0);
+	top: 40%;
+`
+
+export {
+	StyledLoginPopup
+}
