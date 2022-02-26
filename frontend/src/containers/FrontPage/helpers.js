39c5579e52320b8a41b8bafd3e9d21cf015caf48
GRUDE <kristoffenbg@gmail.com>
2022-02-27 00:40:44 +0100

    add documentation to filter helper function

--- frontend/src/containers/FrontPage/helpers.js
+++ frontend/src/containers/FrontPage/helpers.js
@@ -1,8 +1,15 @@
-export const getFilteredRecipes = (recipeData, meal, estimate, cousine, otherCategories) => recipeData.filter(recipe => {
-    if (!meal.lengh && !estimate.lengh && !cousine.lengh && !meal.lengh && !otherCategories.lengh ) return true;
-    return (recipe.meal.some(spesificMeal => meal.includes(spesificMeal)) 
-            || recipe.estimate.some(spesificEstimate => estimate.includes(spesificEstimate)) 
-            || recipe.cousine.some(spesificCousine => cousine.includes(spesificCousine)) 
-            || recipe.otherCategories.some(spesificOtherCategories => otherCategories.includes(spesificOtherCategories))
-    );
-})
+/* 
+Helper function that filters recipes based on categories. If no categories is selected all recepies is returned
+If a category is selected, only recepies matching the categories is returned
+*/
+
+export const getFilteredRecipes = (recipeData, meal, estimate, cousine, otherCategories) => 
+    recipeData.filter(recipe => {
+        if (!meal.lengh && !estimate.lengh && !cousine.lengh && !meal.lengh && !otherCategories.lengh ) return true;
+        return (recipe.meal.some(spesificMeal => meal.includes(spesificMeal)) 
+                || recipe.estimate.some(spesificEstimate => estimate.includes(spesificEstimate)) 
+                || recipe.cousine.some(spesificCousine => cousine.includes(spesificCousine)) 
+                || recipe.otherCategories.some(spesificOtherCategories => otherCategories.includes(spesificOtherCategories))
+        );
+    }
+);
