0416569665cd6eb4ffdf1d6f3abd28ff41ea4454
Kristoffer Nicolay Broch Grude <kngrude@stud.ntnu.no>
2022-03-03 08:34:32 +0100

    Feature/add difficulty as category

--- frontend/src/containers/FrontPage/helpers.js
+++ frontend/src/containers/FrontPage/helpers.js
@@ -3,12 +3,13 @@ Helper function that filters recipes based on categories. If no categories is se
 If a category is selected, only recepies matching the categories is returned
 */
 
-export const getFilteredRecipes = (recipeData, meal, estimate, cousine, otherCategories) => 
+export const getFilteredRecipes = (recipeData, meal, estimate, difficulty ,cousine, otherCategories) => 
     recipeData.filter(recipe => {
         console.log(recipe.meal)
-        if (!meal.length && !estimate.length && !cousine.length && !otherCategories.length ) return true;
+        if (!meal.length && !estimate.length && !difficulty.length && !cousine.length && !otherCategories.length ) return true;
         return (!meal.length ? true : meal.includes(recipe.meal)
                 && !estimate.length ? true : estimate.includes(recipe.estimate) 
+                && !difficulty.length ? true : difficulty.includes(recipe.difficulty)
                 && !cousine.length ? true : cousine.includes(recipe.cousine) 
                 && !otherCategories.length ? true : recipe.categories.some(spesificOtherCategories => otherCategories.includes(spesificOtherCategories))
         );
