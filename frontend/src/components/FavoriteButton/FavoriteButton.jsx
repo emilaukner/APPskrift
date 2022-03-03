0604a67b2d5f69a0db5a20b42c7cab51d1d1290a
Kristoffer Nicolay Broch Grude <kngrude@stud.ntnu.no>
2022-03-03 08:26:09 +0100

    fix like button

--- frontend/src/components/FavoriteButton/FavoriteButton.jsx
+++ frontend/src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,83 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Typography, IconButton } from "@mui/material";
 import { useState } from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import Box from "@mui/material/Box";
 import axios from "axios";
-import { useCookies } from "react-cookie"; 
+import { useCookies } from "react-cookie";
 
-
-
-  
 const FavoriteButton = (props) => {
+  // Byttes ut med props.numberOfLikes    HELP
+  const numberOfLikes = "";
 
-    // Byttes ut med props.likedByUser    HELP
-    const likedByUser = true; 
-    // Byttes ut med props.numberOfLikes    HELP
-    const numberOfLikes = 325; 
-    
-    //React hooks with the states liked and saved
-    const [liked, setLiked] = useState(likedByUser);
-    const [cookie, setCookie] = useCookies(["user"])
+  //React hooks with the states liked and saved
+  const [liked, setLiked] = useState(false);
+  const [cookie, setCookie] = useCookies(["user"]);
 
-    /* Function that is called when user presses like button.
-  If liked = true a delete request is made. If liked = false a post request is made.*/
-    const LikeRecipe = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        liked ? postUnlikeRecipe() : postLikeRecipe();
-      };
+  useEffect(() => {
+    getIsLiked();
+  }, [])
 
-    //API request to like recipe
-    //TODO change hardcoded userId to `${currentUserId}`
-    const postLikeRecipe = async () => {
-      await axios
-        .post(`/users/${cookie.userId}/favorites/`, {
+  /* Function that is called when user presses like button.
+    If liked = true a delete request is made. If liked = false a post request is made.*/
+  const LikeRecipe = (event) => {
+    liked ? postUnlikeRecipe() : postLikeRecipe();
+  };
+
+  const getIsLiked = async () => {
+    await axios
+      .get(`/users/${cookie.userId}/favorites/`)
+      .then((res) => {
+        const likedRecipes = res.data;
+        console.log("Recipe:", props)
+        if(likedRecipes.includes(props.recipeId)) {
+          setLiked(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  //API request to like recipe
+  const postLikeRecipe = async () => {
+    await axios
+      .post(`/users/${cookie.userId}/favorites/`, {
+        id: `${props.recipeId}`,
+      })
+      .then(() => {
+        setLiked(!liked);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  //API request to delete like on a recipe
+  const postUnlikeRecipe = async () => {
+    await axios
+      .delete(`/users/${cookie.userId}/favorites/`, {
+        data: {
           id: `${props.recipeId}`,
-        })
-        .then(() => {
-          setLiked(!liked);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-    
-
-    //API request to delete like on a recipe
-    //TODO change hardcoded userId to `${currentUserId}`
-    const postUnlikeRecipe = async () => {
-        await axios
-        .delete(`/users/${cookie.userId}d/favorites/`, {
-                    data: {
-                        id: `${props.recipeId}`,
-                    }
-        })
-        .then(() => {
-            setLiked(!liked);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    };
+        },
+      })
+      .then(() => {
+        setLiked(!liked);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
       <Box>
         <IconButton
-            onClick={LikeRecipe}
-            aria-label="add to favorites"
-            sx={{ fontSize: "1em", color: "darkgrey" }}
+          onClick={LikeRecipe}
+          aria-label="add to favorites"
+          sx={{ fontSize: "1em", color: "darkgrey" }}
         >
-            {!liked ? (
+          {!liked ? (
             <FavoriteBorderIcon />
-            ) : (
+          ) : (
             <FavoriteIcon sx={{ color: "Crimson" }} />
-            )}
-            <Typography>{numberOfLikes}</Typography>
+          )}
+          <Typography>{numberOfLikes}</Typography>
         </IconButton>
-    </Box>        
+      </Box>
     </>
   );
 };
