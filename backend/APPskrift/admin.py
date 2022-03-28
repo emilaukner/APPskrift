20844c1a35ab3e6b4ca81561cb090a775a913f66
Vegard Sjåvik <vgsjaavik@gmail.com>
2022-03-28 13:21:56 +0200

    Changed column on adminpage

--- backend/APPskrift/admin.py
+++ backend/APPskrift/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+	list_display = ["username", "email"]
 
-admin.site.register(User)
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+	list_display = ["title", "recipeId"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Evaluation)
